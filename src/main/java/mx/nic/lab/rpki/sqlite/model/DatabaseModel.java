052a0f0a6a773ed4f8e6ef35838d75124be1179c
Paco Moreno <pc.moreno2099@gmail.com>
2018-09-21 16:06:07 -0500

    Create model with static and sync'd functions to interact with DB

---
+++ src/main/java/mx/nic/lab/rpki/sqlite/model/DatabaseModel.java
@@ -0,0 +1,87 @@
+package mx.nic.lab.rpki.sqlite.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Main class that SHOULD be used to execute all the queries at DB in a
+ * synchronous way; this is done to avoid an SQLITE_BUSY error when the database
+ * is locked
+ *
+ */
+public class DatabaseModel {
+
+	/**
+	 * Main query to get the last inserted ID
+	 */
+	private static final String GET_LAST_ROWID_SQL = "select last_insert_rowid()";
+
+	/**
+	 * Get a {@link PreparedStatement} from the <code>connection</code> in a
+	 * synchronized manner, the <code>clazz</code> sent is used as a lock
+	 * 
+	 * @param connection
+	 *            DB connection
+	 * @param sql
+	 *            SQL used for the {@link PreparedStatement}
+	 * @param clazz
+	 *            {@link Class} to use as lock
+	 * @return
+	 * @throws SQLException
+	 */
+	public static <T> PreparedStatement prepareStatement(Connection connection, String sql, Class<T> clazz)
+			throws SQLException {
+		synchronized (clazz) {
+			return connection.prepareStatement(sql);
+		}
+	}
+
+	/**
+	 * Return the {@link ResultSet} of the {@link PreparedStatement#executeQuery()}
+	 * using the <code>statement</code> sent and the <code>clazz</code> a lock
+	 * 
+	 * @param statement
+	 * @param clazz
+	 * @return
+	 * @throws SQLException
+	 */
+	public static <T> ResultSet executeQuery(PreparedStatement statement, Class<T> clazz) throws SQLException {
+		synchronized (clazz) {
+			return statement.executeQuery();
+		}
+	}
+
+	/**
+	 * Return the affected rows result of the
+	 * {@link PreparedStatement#executeUpdate()} using the <code>statement</code>
+	 * sent and the <code>clazz</code> a lock
+	 * 
+	 * @param statement
+	 * @param clazz
+	 * @return
+	 * @throws SQLException
+	 */
+	public static <T> int executeUpdate(PreparedStatement statement, Class<T> clazz) throws SQLException {
+		synchronized (clazz) {
+			return statement.executeUpdate();
+		}
+	}
+
+	/**
+	 * Get the last rowid used in an insert statement, using the SQLite
+	 * 'last_insert_rowid()' function
+	 * 
+	 * @param connection
+	 * @param clazz
+	 * @return
+	 * @throws SQLException
+	 */
+	public static <T> Long getLastRowid(Connection connection, Class<T> clazz) throws SQLException {
+		try (PreparedStatement statement = prepareStatement(connection, GET_LAST_ROWID_SQL, clazz)) {
+			ResultSet resultSet = executeQuery(statement, clazz);
+			return resultSet.getLong(1);
+		}
+	}
+}
