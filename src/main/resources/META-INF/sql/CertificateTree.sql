f49c09882838aaf73908a4aa5667ff70e9c3ab18
Paco Moreno <pc.moreno2099@gmail.com>
2018-11-20 14:53:06 -0600

    Don't include self signed CAs

--- src/main/resources/META-INF/sql/CertificateTree.sql
+++ src/main/resources/META-INF/sql/CertificateTree.sql
@@ -1,9 +1,11 @@
 #geFromRoot
 select rpo_id, rpo_type, rpo_subject_key_identifier
   from rpki_object
- where rpo_authority_key_identifier = ?;
+ where rpo_authority_key_identifier = ?
+   and ifnull(rpo_subject_key_identifier, '') != rpo_authority_key_identifier;
 
 #countChilds
 select count(*)
   from rpki_object
- where rpo_authority_key_identifier = ?;
+ where rpo_authority_key_identifier = ?
+   and ifnull(rpo_subject_key_identifier, '') != rpo_authority_key_identifier;
